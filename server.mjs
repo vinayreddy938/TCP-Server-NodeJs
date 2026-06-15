31dbf1e3fbc4d07800b70e737b2d3a9f1086c3bc
vinayreddy938 <vinaykumarreddycse@gmail.com>
2026-06-15 19:00:12 +0530

    successfully receive data from client using mobile app and send response hello from server

--- server.mjs
+++ server.mjs
@@ -2,7 +2,13 @@ import net from "net";
 
 const server = net.createServer((socket)=>{
     console.log("Client connected");
-    console.log(socket);
+    socket.on("data", (data)=>{
+        console.log("Received data from client:", data.toString());
+        socket.write("Hello from server!");
+    });
+    socket.on("end", ()=>{
+        console.log("Client disconnected");
+    });
 });
 server.listen(4000, "0.0.0.0",()=>{  
     console.log("Server listening on port 4000");
